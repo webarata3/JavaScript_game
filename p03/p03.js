2b1a1cce1f2ce52afcc677d3d35dbe24e881a423
webarata3 <webarata3@gmail.com>
2017-11-30 20:05:21 +0900

    p04

--- p03/p03.js
+++ p03/p03.js
@@ -1,7 +1,9 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-document.addEventListener('mousedown', function(e) {
+var isClick = false;
+
+document.addEventListener('mousedown', function (e) {
     // 処理
     if (e.button === 0) {
         var rect = e.target.getBoundingClientRect();
@@ -10,20 +12,24 @@ document.addEventListener('mousedown', function(e) {
 
         ctx.beginPath();
         ctx.moveTo(canvasX, canvasY);
+        isClick = true;
+    }
+});
+
+document.addEventListener('mousemove', function (e) {
+    // 処理
+    if (e.button === 0) {
+        if (!isClick) return;
+
+        var rect = e.target.getBoundingClientRect();
+        var canvasX = e.clientX - rect.left;
+        var canvasY = e.clientY - rect.top;
+
         ctx.lineTo(canvasX + 1, canvasY + 1);
         ctx.stroke();
     }
 });
 
-document.addEventListener('mousemove', function(e) {
-    if (e.button === 0) {
-        var rect = e.target.getBoundingClientRect();
-        var canvasX = e.clientX - rect.left;
-        var canvasY = e.clientY - rect.top;
-
-        ctx.beginPath();
-        ctx.moveTo(canvasX, canvasY);
-        ctx.lineTo(canvasX + 1, canvasY + 1);
-        ctx.stroke();
-    }
+document.addEventListener('mouseup', function (e) {
+    isClick = false;
 });
--- p04/p04.js
+++ p04/p04.js
@@ -1,7 +1,9 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
-document.addEventListener('mousedown', function(e) {
+var isClick = false;
+
+document.addEventListener('mousedown', function (e) {
     // 処理
     if (e.button === 0) {
         var rect = e.target.getBoundingClientRect();
@@ -10,20 +12,31 @@ document.addEventListener('mousedown', function(e) {
 
         ctx.beginPath();
         ctx.moveTo(canvasX, canvasY);
-        ctx.lineTo(canvasX + 1, canvasY + 1);
-        ctx.stroke();
+        isClick = true;
     }
 });
 
-document.addEventListener('mousemove', function(e) {
+document.addEventListener('mousemove', function (e) {
+    // 処理
     if (e.button === 0) {
+        if (!isClick) return;
+
         var rect = e.target.getBoundingClientRect();
         var canvasX = e.clientX - rect.left;
         var canvasY = e.clientY - rect.top;
 
-        ctx.beginPath();
-        ctx.moveTo(canvasX, canvasY);
         ctx.lineTo(canvasX + 1, canvasY + 1);
         ctx.stroke();
     }
 });
+
+document.addEventListener('mouseup', function (e) {
+    isClick = false;
+});
+
+var colorList = document.querySelectorAll('[name="color"]');
+for (var i = 0; i < colorList.length; i++) {
+    colorList[i].addEventListener('click', function(e) {
+        alert(e.target.value);
+    });
+}
