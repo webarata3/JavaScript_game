c64d2648b430c3e366976903b4edea6e8b85771e
webarata3 <webarata3@gmail.com>
2017-11-30 17:00:41 +0900

    p02

---
+++ p02/p02.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+document.addEventListener('mousedown', function(e) {
+    // 処理
+    if (e.button === 0) {
+        var rect = e.target.getBoundingClientRect();
+        var canvasX = e.clientX - rect.left;
+        var canvasY = e.clientY - rect.top;
+
+        ctx.beginPath();
+        ctx.moveTo(canvasX, canvasY);
+        ctx.lineTo(canvasX + 1, canvasY + 1);
+        ctx.stroke();
+    }
+});
