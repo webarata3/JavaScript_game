8d6031f75b5114d0722fda11d0c2b8058c3a1753
webarata3 <webarata3@gmail.com>
2017-12-30 09:34:40 +0900

    スコアの実装

--- tamayoke3/t/tamayoke.js
+++ tamayoke3/t/tamayoke.js
@@ -9,6 +9,8 @@ class Robot {
 
         this._pushLeft = false;
         this._pushRight = false;
+
+        this._hit = false;
     }
 
     getX() {
@@ -48,6 +50,19 @@ class Robot {
             }
         }
     }
+
+    checkHit(missile) {
+        if (!((this._x > missile.getX() + Missile.WIDTH
+                || this._x + Robot.WIDTH < missile.getX())
+                || (this._y > missile.getY() + Missile.HEIGHT
+                    || this._y + Robot.HEIGHT < missile.getY()))) {
+            this._hit = true;
+        }
+    }
+
+    isHit() {
+        return this._hit;
+    }
 }
 
 Robot.WIDTH = 30;
@@ -55,10 +70,46 @@ Robot.HEIGHT = 30;
 Robot.SPEED = 5;
 
 class Missile {
-    constructor() {
+    constructor(canvasWidth, canvasHeight, initY) {
+        this._canvasWidth = canvasWidth;
+        this._canvasHeight = canvasHeight;
+        this._x = this._randomX();
+        this._y = initY;
+    }
 
+    getX() {
+        return this._x;
+    }
+
+    getY() {
+        return this._y;
+    }
+
+    move() {
+        this._y = this._y + Missile.speed;
+        if (this._y > this._canvasHeight) {
+            this._y = -Missile.speed;
+
+            // xも変える。
+            this._x = this._randomX();
+
+            // ミサイルがリセットされたらtrue
+            return true;
+        }
+        // ミサイルがりせっとされていない
+        return false;
+    }
+
+    _randomX() {
+        return Math.floor(Math.random() * (this._canvasWidth - Missile.WIDTH));
     }
 }
+
+Missile.WIDTH = 5;
+Missile.HEIGHT = 20;
+Missile.INIT_SPEED = 3;
+Missile.INCREASE_SPEED = 1.08;
+Missile.NUM = 5;
 
 class Model {
     constructor(canvasWidth, canvasHeight) {
@@ -66,6 +117,13 @@ class Model {
         this._canvasHeight = canvasHeight;
 
         this._robot = new Robot(this._canvasWidth, this._canvasHeight);
+        this._missiles = [];
+        for (let i = 0; i < Missile.NUM; i++) {
+            this._missiles[i] = new Missile(this._canvasWidth, this._canvasHeight, -i * (Missile.HEIGHT + 40));
+        }
+
+        this._score = 0;
+        Missile.speed = Missile.INIT_SPEED;
     }
 
     getRobot() {
@@ -91,6 +149,36 @@ class Model {
     pushRight() {
         this._robot.pushRight();
     }
+
+    getMissiles() {
+        return this._missiles;
+    }
+
+    getScore() {
+        return this._score;
+    }
+
+    moveMissiles() {
+        for (const missile of this._missiles) {
+            // ミサイルが最下部まで行ったら得点を1足す
+            if (missile.move()) {
+                this._score = this._score + 1;
+                if (this._score % 10 === 0) {
+                    Missile.speed = Missile.speed * Missile.INCREASE_SPEED;
+                }
+            }
+        }
+    }
+
+    checkHit() {
+        for (const missile of this._missiles) {
+            this._robot.checkHit(missile);
+        }
+    }
+
+    isHit() {
+        return this._robot.isHit();
+    }
 }
 
 class View {
@@ -101,15 +189,37 @@ class View {
         this._model = model;
     }
 
-    update() {
-        this._model.moveRobot();
+    start() {
+        this._timer = setInterval(() => {
+            this._update();
+        }, 1000 / 60);
 
+    }
+
+    _update() {
+        this._main();
         this._draw();
+    }
+
+    _main() {
+        this._model.moveRobot();
+        this._model.moveMissiles();
+
+        this._model.checkHit();
     }
 
     _draw() {
         this._clearCanvas();
         this._drawRobot();
+        this._drawMissiles();
+        this._drawScore();
+
+        if (this._model.isHit()) {
+            clearInterval(this._timer);
+
+            this._drawRestartMessage();
+        }
+
     }
 
     _clearCanvas() {
@@ -122,6 +232,36 @@ class View {
         this._ctx.fillStyle = '#00f';
         this._ctx.fillRect(robot.getX(), robot.getY(), Robot.WIDTH, Robot.HEIGHT);
     }
+
+    _drawMissiles() {
+        for (const missile of this._model.getMissiles()) {
+            this._drawMissile(missile);
+        }
+    }
+
+    _drawMissile(missile) {
+        this._ctx.fillStyle = '#f00';
+        this._ctx.fillRect(missile.getX(), missile.getY(), Missile.WIDTH, Missile.HEIGHT);
+    }
+
+    _drawScore() {
+        this._ctx.fillStyle = '#000';
+        this._ctx.font = '10px sans-serif';
+        this._ctx.fillText('スコア: ' + this._model.getScore(), 5, 30);
+    }
+
+    _drawRestartMessage() {
+        this._ctx.font = '20px sans-serif';
+        this._ctx.fillStyle = '#f00';
+        this._ctx.strokeStyle = '#000';
+        this._fillTextCenter('Game Over', 100);
+        this._fillTextCenter('スペースキーでリスタート', 150);
+    }
+
+    _fillTextCenter(text, y) {
+        const metrics = this._ctx.measureText(text);
+        this._ctx.fillText(text, (this._canvas.width - metrics.width) / 2, y);
+    }
 }
 
 class Controller {
@@ -129,10 +269,14 @@ class Controller {
         this._model = new Model(canvasWidth, canvasHeight);
         this._view = new View(canvasId, this._model);
 
-        window.addEventListener('keyup', e => { this._keyup(e) });
-        window.addEventListener('keydown', e => { this._keydown(e) });
+        window.addEventListener('keyup', e => {
+            this._keyup(e)
+        });
+        window.addEventListener('keydown', e => {
+            this._keydown(e)
+        });
 
-        const timer = setInterval(() => { this._view.update(); }, 1000 / 60);
+        this._view.start();
     }
 
     _keyup(e) {
